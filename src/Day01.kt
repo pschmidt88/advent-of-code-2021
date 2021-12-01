334572873143dd79606e4e861d14d90cd76fd4af
Paul Schmidt <rookian@gmail.com>
2021-12-01 22:01:12 +0100

    Solutions for day 1
    Don't make fun of code. I was tired and hungry :(

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var lastScan: Int? = null
+        var counter = 0
+
+        input
+            .map { it.toInt() }
+            .forEach { currentScan ->
+                lastScan?.let {
+                if (currentScan > it) {
+                    counter++
+                }
+            }
+
+            lastScan = currentScan
+        }
+
+        return counter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sumList: MutableList<String> = mutableListOf()
+        val intList = input.map(String::toInt)
+
+        intList.mapIndexed { index, currentScan ->
+            if (intList.size > index+2) {
+                sumList.add((currentScan + intList[index+1] + intList[index+2]).toString())
+            }
+        }
+
+        return part1(sumList)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
